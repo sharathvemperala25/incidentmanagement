48dcfe6a8628573a75fedcfcae9ea7256ab1f4b9
pnvijay123 <pnvijay.research@gmail.com>
2019-05-28 18:11:43 +0530

    Committing Test Case for UserIncidenceRepository

---
+++ src/test/java/com/hcl/incidentmanagement/UserIncidentsRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.hcl.incidentmanagement.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.hcl.incidentmanagement.repository.UserIncidentsRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserIncidentsRepositoryTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private UserIncidentsRepository userIncidentsRepository;
+
+	@Before
+	public void deleteAllBeforeTests() throws Exception {
+		userIncidentsRepository.deleteAll();
+	}
+
+	@Test
+	public void shouldReturnRepositoryIndex() throws Exception {
+		mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(
+				jsonPath("$._links.ims").exists());
+	}
+}
