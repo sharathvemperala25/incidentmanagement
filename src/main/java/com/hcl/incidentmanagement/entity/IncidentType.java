d950fa2a1b0a3a6a8bf990b3f70e37ee9cc15c11
nithin2889 <nithin2889@gmail.com>
2019-05-28 14:31:12 +0530

    user pojo

--- src/main/java/com/hcl/incidentmanagement/entity/IncidentType.java
+++ src/main/java/com/hcl/incidentmanagement/entity/IncidentType.java
@@ -1,12 +1,13 @@
 package com.hcl.incidentmanagement.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -18,22 +19,16 @@ import lombok.Setter;
 @Entity
 @Table
 @Setter @Getter @NoArgsConstructor
-public class Incident {
+
+public class IncidentType {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name="Incident_ownerid")
+	private String IncidentType;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy="owner")
 	@JsonIgnore
-	private User owner;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name="Incident_Type")
-	@JsonIgnore
-	private IncidentType incidentType;
-	
-	
-	
+	private List<Incident> incidents;
 
 }
