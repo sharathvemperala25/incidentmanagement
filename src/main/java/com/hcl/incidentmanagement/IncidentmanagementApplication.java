b37868862b6b62efaf686e170018dddb07ee366f
nithin2889 <nithin2889@gmail.com>
2019-05-28 12:59:40 +0530

    initial setup

---
+++ src/main/java/com/hcl/incidentmanagement/IncidentmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.incidentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentmanagementApplication.class, args);
+	}
+
+}
